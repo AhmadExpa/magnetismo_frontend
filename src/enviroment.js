4768d3647a01fce7c353335ce1462d49fac61111
AhmadExpa <ahmadexpa@gmail.com>
2025-08-20 01:48:47 +0500

    Update enviroment.js

--- src/enviroment.js
+++ src/enviroment.js
@@ -1,18 +1,18 @@
-export const VITE_HOME_HERO_SECTION_VIDEO_URL = "https://res.cloudinary.com/ddtqjxuok/video/upload/v1752040407/headerhd_lkzsaq.mp4"
+export const VITE_HOME_HERO_SECTION_VIDEO_URL = "https://res.cloudinary.com/ddtnvnuzi/video/upload/v1755636118/Magnetismo_Animattion_HD_vblxmd.mp4"
 export const VITE_HOME_HERO_SECTION_VIDEO2_URL = "https://res.cloudinary.com/ddtqjxuok/video/upload/v1752040467/long_w17xzk.mp4"
 export const VITE_VIDEO_IN_HOME_SCREENTIME_SECTION = "https://res.cloudinary.com/ddtqjxuok/video/upload/v1752043045/screentimevideo_z1dndj.mp4"
-export const VITE_PRICE_VIDEO_IN_INNOVATIVE_AND_HOW_IT_WORKS_PAGE = "https://res.cloudinary.com/ddtqjxuok/video/upload/v1752040629/pricevideo_hypkag.mp4"
+export const VITE_PRICE_VIDEO_IN_INNOVATIVE_AND_HOW_IT_WORKS_PAGE = "https://res.cloudinary.com/ddtnvnuzi/video/upload/v1755636121/Magnetismo_Animation_3D_Girl_oq6epk.mp4"
 
 
 export const VITE_TESTIMONIALS_SECTION_JESSICA_VIDEO_URL = "https://res.cloudinary.com/ddtqjxuok/video/upload/v1752042924/jessicalatestvideo_epucmf.mp4"
 export const VITE_TESTIMONIALS_SECTION_JACK_VIDEO_URL = "https://res.cloudinary.com/ddtqjxuok/video/upload/v1752040649/zackkvideo_zoyfhd.mp4"
 export const VITE_TESTIMONIALS_SECTION_ZACK_VIDEO_URL = "https://res.cloudinary.com/ddtqjxuok/video/upload/v1752040619/jackkvideo_pssv7a.mp4"
 
-export const VITE_FOOTER_SECTION_VIDEO_URL = "https://res.cloudinary.com/ddtqjxuok/video/upload/v1752040503/footer_klbdv5.mp4"
+export const VITE_FOOTER_SECTION_VIDEO_URL = "https://res.cloudinary.com/ddtnvnuzi/video/upload/v1755636118/Magnetismo_Animattion_HD_vblxmd.mp4"
 
-export const VITE_STORY_HERO_SECTION_VIDEO_URL = "https://res.cloudinary.com/ddtqjxuok/video/upload/v1752042129/Magnetismo_Animattion_HD_Vertical_V2_xhohln.mp4"
-export const VITE_HOW_IT_WORKS_HERO_SECTION_VIDEO_URL = "https://res.cloudinary.com/ddtqjxuok/video/upload/v1752040554/howitworksnewvideo_hwvbkj.mp4"
-export const VITE_HOW_IT_WORKS_HERO_SECTION_VIDEO2_URL = "https://res.cloudinary.com/ddtqjxuok/video/upload/v1752040554/howitworksnewvideo_hwvbkj.mp4"
+export const VITE_STORY_HERO_SECTION_VIDEO_URL = "https://res.cloudinary.com/ddtnvnuzi/video/upload/v1755636114/Magnetismo_Animattion_HD_Vertical_V2_u3ixpz.mp4"
+export const VITE_HOW_IT_WORKS_HERO_SECTION_VIDEO_URL = "https://res.cloudinary.com/ddtnvnuzi/video/upload/v1755636121/Magnetismo_Animation_3D_Girl_oq6epk.mp4"
+export const VITE_HOW_IT_WORKS_HERO_SECTION_VIDEO2_URL = "https://res.cloudinary.com/ddtnvnuzi/video/upload/v1755636121/Magnetismo_Animation_3D_Girl_oq6epk.mp4"
 
 export const VITE_INNOVATIVE_SECTION_VIDEO_URL = "https://res.cloudinary.com/ddtqjxuok/video/upload/v1752040629/pricevideo_hypkag.mp4"
 export const VITE_PRODUCT_HERO_SECTION_VIDEO_URL = "https://res.cloudinary.com/ddtqjxuok/video/upload/v1752040721/pHero_nu4khj.mp4"
